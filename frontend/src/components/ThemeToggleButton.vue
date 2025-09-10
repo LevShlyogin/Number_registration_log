0c862453100c67afa36764a828d08062653ab787
Misha-Mayskiy <Shofer2007@gmail.com>
2025-09-10 12:37:55 +0300

    fix: AdminView.vue page init, toggle theme button upgrade method

--- frontend/src/components/ThemeToggleButton.vue
+++ frontend/src/components/ThemeToggleButton.vue
@@ -13,6 +13,6 @@ import { useTheme } from 'vuetify'
 const theme = useTheme()
 
 function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  theme.change(theme.global.current.value.dark ? 'light' : 'dark')
 }
 </script>
---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container>
+    <h1 class="text-h4 font-weight-bold">Администрирование</h1>
+    <p class="mt-4">Этот раздел находится в разработке.</p>
+  </v-container>
+</template>
+
+<script setup lang="ts"></script>
